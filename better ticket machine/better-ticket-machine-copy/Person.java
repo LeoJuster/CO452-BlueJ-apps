e26c46cf25b7a42daa0742172c694ba08394f14d
Leopold Juster <21900080@bucks.ac.uk>
2020-10-12 20:30:23 +0100

    In this assingment, i have created two new classes and added methods and parameters to them. and i have also added an (amount left to pay) sectio along with an empty method.

---
+++ better ticket machine/better-ticket-machine-copy/Person.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Person here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // The name of the person.
+    private int myName;
+    // This age of the person.
+    private int myAge ;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person(int myAge)
+    {
+        // initialise instance variables
+        String myName;
+   
+    
+        myAge = myAge;
+    }
+    public void myAge()
+    {
+         System.out.println (myAge);
+    }
+   
+}
